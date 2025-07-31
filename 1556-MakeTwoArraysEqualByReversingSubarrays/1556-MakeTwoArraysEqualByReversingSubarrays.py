e451a589b41a6717da9925aff9e72d619c140a7c
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:12:18 +0530

    Create 1556-MakeTwoArraysEqualByReversingSubarrays

---
+++ 1556-MakeTwoArraysEqualByReversingSubarrays/1556-MakeTwoArraysEqualByReversingSubarrays.py
@@ -0,0 +1,8 @@
+# Last updated: 31/07/2025, 21:12:15
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        return sorted(target)==sorted(arr)              
+
+
+
+        
