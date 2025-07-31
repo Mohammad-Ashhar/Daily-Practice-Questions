154fcc994cc22563ebf76965e7c5ae582ad4a9cd
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:16:02 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 31/07/2025, 21:15:59
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+       switch (num) {
+      case 6 : return true;
+      case 28 : return true;
+      case 496 : return true;
+      case 8128 : return true;
+      case 33550336 : return true;
+      default : return false;
+    } 
+    }
+}
