b9ff7376634261ffa67dfe1523e06e201461b0ce
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:19:08 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.java
@@ -0,0 +1,16 @@
+// Last updated: 31/07/2025, 21:19:05
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 1; 
+        return result;
+    }
+}
