5d7a627c33781b32b54d8540a8744691e43a8771
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:12:08 +0530

    Create 1580-ShuffleTheArray

---
+++ 1580-ShuffleTheArray/1580-ShuffleTheArray.py
@@ -0,0 +1,15 @@
+# Last updated: 31/07/2025, 21:12:05
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res=[]
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[i+n])
+        return res    
+
+
+        
+    
+        
+
+        
