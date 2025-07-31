3d44211c34fc8a72e112ac8396f5f075040b57d8
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:06:09 +0530

    Create 3215-MatrixSimilarityAfterCyclicShifts

---
+++ 3215-MatrixSimilarityAfterCyclicShifts/3215-MatrixSimilarityAfterCyclicShifts.java
@@ -0,0 +1,18 @@
+// Last updated: 31/07/2025, 21:06:06
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+           int m=mat.length;
+int n=mat[0].length;
+for(int i=0;i<m;i++)
+{
+	for(int j=0;j<n;j++)
+	{
+		if(mat[i][j] != mat[i][(j+k)%n])
+		{
+			return false;
+		}
+	}
+}
+return true;
+    }
+}
