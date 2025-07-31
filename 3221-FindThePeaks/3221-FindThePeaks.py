a56b7ad5238f33bc49083b3e0b9378881da4b48d
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:06:06 +0530

    Create 3221-FindThePeaks

---
+++ 3221-FindThePeaks/3221-FindThePeaks.py
@@ -0,0 +1,11 @@
+# Last updated: 31/07/2025, 21:06:03
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        arr=[]
+        for i in range(1,len(mountain)-1):
+            if mountain[i]>mountain[i-1] and mountain[i]>mountain[i+1]:
+                arr.append(i)
+        return arr        
+
+
+        
