9b2b32184e9c479f403951897175f944ad0bcad3
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:14:53 +0530

    Create 866-RectangleOverlap

---
+++ 866-RectangleOverlap/866-RectangleOverlap.java
@@ -0,0 +1,26 @@
+// Last updated: 31/07/2025, 21:14:50
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        if(rec1[2]<=rec2[0])
+        {
+            return false;
+        }
+        else if(rec1[0]>=rec2[2])
+        {
+            return false;
+        }
+        else if(rec1[1]>=rec2[3])
+        {
+            return false;
+        }
+        else if(rec1[3]<=rec2[1])
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+        
+    }
+}
