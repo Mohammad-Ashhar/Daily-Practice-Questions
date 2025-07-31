43c0f32427fb5fd040583b530e25037755dda65b
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:11:49 +0530

    Create 1630-CountOddNumbersInAnIntervalRange

---
+++ 1630-CountOddNumbersInAnIntervalRange/1630-CountOddNumbersInAnIntervalRange.java
@@ -0,0 +1,12 @@
+// Last updated: 31/07/2025, 21:11:46
+class Solution {
+    public int countOdds(int low, int high) {
+        int c=(high-low)/2;
+        if(low%2!=0||high%2!=0)
+        {
+            c++;
+        }
+        return c;
+        
+    }
+}
