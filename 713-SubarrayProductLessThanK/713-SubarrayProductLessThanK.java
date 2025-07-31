478862d9c2513bd79e99faaee7f72cf1784df6b2
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:15:28 +0530

    Create 713-SubarrayProductLessThanK

---
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
@@ -0,0 +1,28 @@
+// Last updated: 31/07/2025, 21:15:25
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        
+         return product_less_than_k(nums,k);
+        
+        }
+        public static int product_less_than_k(int[] nums,int k)
+        {
+            int ans=0,si=0,ei=0,p=1;
+            while(ei<nums.length)
+            {
+                // grow
+                p=p*nums[ei];
+                // shrink
+                while(p>=k && si<=ei)
+                {
+                    p=p/nums[si];
+                    si++;
+                }
+                // ans update
+                ans=ans+(ei-si +1);
+                ei++;
+            }
+            return ans;
+        }
+        
+    }
