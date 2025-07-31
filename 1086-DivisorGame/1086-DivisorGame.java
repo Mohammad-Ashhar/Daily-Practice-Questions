ca4c3bde72a676359d4ae5d7f58edb25340f558b
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:14:03 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,10 @@
+// Last updated: 31/07/2025, 21:14:00
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
