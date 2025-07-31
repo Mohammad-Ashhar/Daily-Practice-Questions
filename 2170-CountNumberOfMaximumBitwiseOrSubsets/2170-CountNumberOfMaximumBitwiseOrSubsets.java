97d897ae2fc53ba199d7204801df9ad520d07143
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:09:47 +0530

    Create 2170-CountNumberOfMaximumBitwiseOrSubsets

---
+++ 2170-CountNumberOfMaximumBitwiseOrSubsets/2170-CountNumberOfMaximumBitwiseOrSubsets.java
@@ -0,0 +1,26 @@
+// Last updated: 31/07/2025, 21:09:44
+class Solution {
+        private int maxOr=0;
+        private int count=0;
+    public int countMaxOrSubsets(int[] nums) {
+        for(int num:nums)
+        {
+            maxOr |=num;
+        }
+        backtrack(nums,0,0);
+        return count;        
+    }
+    private void backtrack(int[] nums,int idx,int curr)
+    {
+        if(idx==nums.length)
+        {
+            if(curr==maxOr)
+            {
+                count++;
+            }
+            return;
+        }
+        backtrack(nums,idx+1,curr |nums[idx]);
+        backtrack(nums,idx+1,curr);
+    }
+}
