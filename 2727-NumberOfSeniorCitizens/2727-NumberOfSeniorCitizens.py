c14fcb4b95ec3a3ef88a24a58282ca09367b03df
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:07:19 +0530

    Create 2727-NumberOfSeniorCitizens

---
+++ 2727-NumberOfSeniorCitizens/2727-NumberOfSeniorCitizens.py
@@ -0,0 +1,9 @@
+# Last updated: 31/07/2025, 21:07:16
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        c=0
+        for i in details:
+            if int(i[11:13])>60:
+                c+=1
+        return c        
+        
