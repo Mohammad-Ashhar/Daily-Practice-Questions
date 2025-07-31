dab47242cfad90940aceef2668255e3fc4a0e48b
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:09:39 +0530

    Create 2195-TimeNeededToBuyTickets

---
+++ 2195-TimeNeededToBuyTickets/2195-TimeNeededToBuyTickets.py
@@ -0,0 +1,18 @@
+# Last updated: 31/07/2025, 21:09:36
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        c=0
+        while True:
+            if tickets[k]==0:
+                break
+            for i in range(len(tickets)):
+                if tickets[k]==0:
+                    break
+                if tickets[i]>0:
+                    tickets[i]=tickets[i]-1
+                    c+=1
+        return c
+
+
+
+        
