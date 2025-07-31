32af43b89faaffd3743b5ebfae14f4f7934fe281
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-07-31 21:08:46 +0530

    Create 2401-CountAsterisks

---
+++ 2401-CountAsterisks/2401-CountAsterisks.java
@@ -0,0 +1,23 @@
+// Last updated: 31/07/2025, 21:08:43
+class Solution {
+    public int countAsterisks(String s) {
+        int ans=0;
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='|')
+            {
+                c++;
+            }
+            if(c%2==0)
+            {
+                if(s.charAt(i)=='*')
+                {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
