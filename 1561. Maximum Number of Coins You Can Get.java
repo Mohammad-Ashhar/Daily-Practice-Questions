754b87af610dc8cd8c1e76c7842b2588ff4256fd
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-08-27 09:40:02 +0530

    Add files via upload

---
+++ 1561. Maximum Number of Coins You Can Get.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int sum=0;
+        int n=piles.length;
+        for(int i=n-2;i>=n/3;i-=2) sum+=piles[i];
+        return sum;
+        
+    }
+}
