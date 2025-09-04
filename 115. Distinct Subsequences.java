7a4ca4d877728dbdda51964e936cfdc1e136a1f5
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-09-04 12:32:36 +0530

    Add files via upload

---
+++ 115. Distinct Subsequences.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int[][] dp=new int[s.length()][t.length()];
+        for(int[] a:dp) Arrays.fill(a,-1);
+       return Num_Distinct(s,t,0,0,dp); 
+    }
+    public static int Num_Distinct(String s,String t,int i,int j,int[][] dp)
+    {
+        if(j==t.length()) return 1;
+        if(i==s.length()) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int inc=0,exc=0;
+        if(s.charAt(i)==t.charAt(j)){
+            inc=Num_Distinct(s,t,i+1,j+1,dp);
+        }
+        exc=Num_Distinct(s,t,i+1,j,dp);
+        return dp[i][j]=inc+exc;
+    }
+}
