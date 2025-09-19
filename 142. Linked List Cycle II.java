16a5f34395c94ecc6d0215b327118abac34d6858
Mohammad Ashhar <142567321+MohammadAshhar@users.noreply.github.com>
2025-09-19 21:00:38 +0530

    Add files via upload

---
+++ 142. Linked List Cycle II.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast=head,slow=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+            {
+                ListNode a=head;
+                ListNode b=slow;
+                while(a!=b)
+                {
+                    a=a.next;
+                    b=b.next;
+                }
+                return a;
+            }
+        }
+        return null;
+        
+    }
+}
