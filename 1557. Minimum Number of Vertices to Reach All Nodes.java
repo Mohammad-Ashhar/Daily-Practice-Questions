afb83b8d6f262db7bb41c608abb94bdfe27e2361
Mohammad Ashhar <142567321+Mohammad-Ashhar@users.noreply.github.com>
2025-10-11 15:26:47 +0530

    Add files via upload

---
+++ 1557. Minimum Number of Vertices to Reach All Nodes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        int[] indegree=new int[n];
+        for(List<Integer>ll:edges)
+        {
+            indegree[ll.get(1)]++;
+        }
+        List<Integer>ll=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0) ll.add(i);
+        }
+        return ll;
+        
+    }
+}
